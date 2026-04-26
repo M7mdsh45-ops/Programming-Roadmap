9109d9f3fd564f1c062c9b89f91806e300b542c6
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-04-26 09:53:19 +0300

    Add driver hiring decision program

---
+++ 04-Algorithms_and_Problem-Solving–Level_1/hire_a_driver_case2.cpp
@@ -0,0 +1,53 @@
+// roadmap_problems_1_to_5.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct stpersoninfo
+{
+	int age;
+	bool driverlicence;
+	bool hasRecommendation;
+};
+
+stpersoninfo readinfo ()
+{
+	stpersoninfo personinfo;
+	cout << "please enter your age : \n";
+	cin >> personinfo.age ;
+	cout << " Do you have a Valid driver licence ? ( 1 for Yes , 0 for No ) \n";
+	cin >> personinfo.driverlicence;
+	cout << " Do you have a recommendation ? \n";
+	cin >> personinfo.hasRecommendation;
+
+	return personinfo ;
+}
+
+bool checkinfo( stpersoninfo personinfo )
+{    
+	return ((personinfo.age > 21 && personinfo.driverlicence) || personinfo.hasRecommendation);
+}
+
+void printresult ( bool check )
+{
+
+	if ( check )
+		cout << " \n HIRED \n";
+	
+
+
+	else
+		cout << "\n REJECTED \n ";
+
+}
+
+int main()
+{
+	printresult(checkinfo(readinfo()));
+
+}
+
+
+
