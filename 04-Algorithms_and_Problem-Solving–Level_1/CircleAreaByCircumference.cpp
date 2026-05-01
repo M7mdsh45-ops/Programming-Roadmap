f2284219f3d3380359333f3736efa2cd86697602
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-05-01 11:24:29 +0300

    Add CircleAreaByCircumference.cpp to calculate area

---
+++ 04-Algorithms_and_Problem-Solving–Level_1/CircleAreaByCircumference.cpp
@@ -0,0 +1,47 @@
+// roadmap_p21.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>   
+#include <string>     
+
+using namespace std;  
+
+
+float ReadCircumference()
+{
+    float L;  
+
+    
+    cout << "Please enter Circumference? \n";
+    cin >> L;
+
+    return L; 
+}
+
+
+float CircleAreaByCircumference(float L)
+{
+    const float PI = 3.141592653589793238; 
+
+    
+    float Area = pow(L, 2) / (4 * PI);
+
+    return Area;  
+}
+
+
+void PrintResult(float Area)
+{
+    
+    cout << "\nCircle Area = " << Area << endl;
+}
+
+
+int main()
+{
+    
+    PrintResult(CircleAreaByCircumference(ReadCircumference()));
+
+    return 0;  
+}
