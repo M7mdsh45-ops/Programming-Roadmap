9f87dc830aa0dedd17c2c49665a2133d1948d0ab
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-05-02 06:22:07 +0300

    Create print_sum_of_even_nums_from_1_to_N.cpp

---
+++ 04-Algorithms_and_Problem-Solving–Level_1/print_sum_of_even_nums_from_1_to_N.cpp
@@ -0,0 +1,47 @@
+// roadmap_p28.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+enum enOddOrEven { odd = 1 , even = 2 };
+
+int readNum()
+{
+    int number;
+    cout << " Please enter a number : \n";
+    cin >> number;
+    return number;
+}
+
+
+
+enOddOrEven CheckOddOrEven(int number)
+{
+    if (number % 2 != 0)
+        return enOddOrEven::odd;
+    else
+        return enOddOrEven::even;
+}
+
+
+int CalcSumOfEvenNumsFrom1ToN(int N)
+{
+    int sum = 0;
+
+    cout << " The sum of even numbers in range = ";
+    for (int counter = 1; counter <= N; counter++)
+    {
+        if (CheckOddOrEven(counter) == enOddOrEven::even)
+            sum += counter;
+    }
+   
+    return sum;
+}
+
+int main()
+{
+   cout << CalcSumOfEvenNumsFrom1ToN(readNum());
+     
+}
+
