e3af51b79fea276db765e0dddd59c7c8cb57a59f
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-05-03 11:53:33 +0300

    Add print_grade_letter.cpp to determine grade letters

---
+++ 04-Algorithms_and_Problem-Solving–Level_1/print_grade_letter.cpp
@@ -0,0 +1,50 @@
+// roadmap_p31.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int readgrade( int from , int to)
+{
+	int grade;
+	do
+	{
+		cout << " Please , Enter your grade : \n";
+		cin >> grade;
+
+	} while (grade < from || grade > to);
+
+	return grade;
+}
+
+
+char GetGradeLetter(int grade)
+{
+	if (grade >= 90)
+		return 'A';
+	else if (grade >= 80)
+		return 'B';
+		
+	else if (grade >= 70)
+		return 'C';
+		
+	else if (grade >= 60)
+		return 'D';
+		
+	else if (grade >= 50)
+		return 'E';
+		
+	else 
+		return 'F';
+		
+
+}
+
+
+int main()
+{
+	int dispaly = readgrade(0, 100);
+	cout << " You got " << GetGradeLetter(dispaly);
+
+}
+
