b92e9dc83abb4c5f1a1cd234b668127407f4c1ca
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-05-07 18:48:50 +0300

    Add program to print all letter combinations
    This program prints all character combinations for each letter from A to Z.

---
+++ 03-CPP_level_1/print_all_letters_potential.cpp
@@ -0,0 +1,27 @@
+// print_all_character_potentials.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+	for (int i = 'A'; i <= 'Z'; i++)
+	{
+		cout << "ALL potentials for " << char(i) << "\n \n";
+
+		for (int j = 'A'; j <= 'Z'; j++)
+		{
+			cout << char(i) << char(j) << "\n";
+
+		}
+		cout << "\n------------------------------------- \n";
+
+
+    }
+
+
+
+
+}
+
