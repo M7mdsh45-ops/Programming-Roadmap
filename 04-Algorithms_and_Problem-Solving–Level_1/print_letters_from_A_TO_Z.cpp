47bc7d1c11d226fd12bd095462317ddbe8f26428
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-05-08 17:58:09 +0300

    Add program to print letters from A to Z

---
+++ 04-Algorithms_and_Problem-Solving–Level_1/print_letters_from_A_TO_Z.cpp
@@ -0,0 +1,22 @@
+// roadmap_p46 
+
+#include<iostream>
+using namespace std;
+
+void printLetters ()
+{
+	for ( int i = 65 ; i <= 90 ; i ++)
+	{
+		cout << char(i) << endl;
+	}
+
+}
+
+
+
+
+int main()
+{
+	printLetters();
+
+}
