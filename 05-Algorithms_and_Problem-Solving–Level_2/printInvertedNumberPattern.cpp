93a41668f79063a2c45c174522b8c725c16bcff7
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-07-19 04:33:07 +0300

    Create printInvertedNumberPattern.cpp

---
+++ 05-Algorithms_and_Problem-Solving–Level_2/printInvertedNumberPattern.cpp
@@ -0,0 +1,51 @@
+// testttt.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+int readNum(string message)
+{
+	int num = 0 ;
+
+	do
+	{
+		cout << message << endl;
+		cin >> num ;
+
+	} while (num <= 0);
+	
+	return num;
+}
+
+void printInvertedNumberPattern ( int num)
+{
+	for (int i = num ; i > 0 ; i--)
+	{
+		
+	    
+		for (int j = 1 ; j <= i ; j++ )
+		{
+			cout << i;
+	
+		}
+		
+		cout << endl;
+	}
+
+
+}
+
+
+
+int main()
+{
+	
+	
+	printInvertedNumberPattern(readNum(" enter a num : "));
+
+
+}
+
