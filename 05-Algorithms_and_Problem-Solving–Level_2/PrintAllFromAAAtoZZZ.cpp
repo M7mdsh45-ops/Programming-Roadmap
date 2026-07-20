73fbb684208acb874e8862dab2ebb18d3f495b24
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-07-20 06:38:13 +0300

    Add function to print all combinations from AAA to ZZZ

---
+++ 05-Algorithms_and_Problem-Solving–Level_2/PrintAllFromAAAtoZZZ.cpp
@@ -0,0 +1,31 @@
+// testttt.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+void PrintAllFromAAAtoZZZ()
+{
+	for (int i = 65 ; i <= 90 ; i++)
+	{
+		
+	    
+		for (int j = 65 ; j <= 90; j++ )
+		{
+			for (int k = 65; k <= 90; k++)
+			{
+				cout << char(i) << char (j) << char (k) << endl;
+    
+			}
+		}
+	}
+
+}
+
+
+int main()
+{
+	PrintAllFromAAAtoZZZ();
+}
+
