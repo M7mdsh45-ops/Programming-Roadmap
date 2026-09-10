b0c85f34adb52256446d07beb3c3e5b1793de7c0
A F C <A F C@DESKTOP-Q5D1QF9>
2026-09-10 20:01:19 +0300

    Add project files.

---
+++ ConsoleApplication24/MyInput.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+namespace MyInput
+{
+
+	int readNum()
+	{
+		int num; 
+
+		cout << "\n\nPlease enter a number \n\n";
+		cin >> num;
+
+		return num;
+	}
+
+
+}
+
+
