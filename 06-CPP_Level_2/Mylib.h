a7f007eb5a24e5566e0691efa8af801ef0a3bdae
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-09-10 20:39:54 +0300

    Add files via upload

---
+++ 06-CPP_Level_2/Mylib.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+namespace MyLib
+{
+
+	void test()
+	{
+		cout << "This Is My First Lib \n\n";
+	}
+
+	int MySum(int a, int b)
+	{
+		return a + b;
+	}
+
+}
+
+
---
+++ 06-CPP_Level_2/myFirstLibs.cpp
@@ -0,0 +1,22 @@
+// myFirstLibs.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "MyLib.h"
+#include "MyInput.h"
+
+using namespace std;
+
+
+int main()
+{
+
+	MyLib::test();
+
+	cout << MyLib::MySum(10, 5);
+
+	int num = MyInput::readNum();
+	cout << "\nThe Number you have entered is " << num << endl;
+
+}
+
