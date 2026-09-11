44e9d80f6cdcf30304643743d829bca1126e3010
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-09-11 16:02:55 +0300

    Ranged_Loops.cpp

---
+++ 06-CPP_Level_2/Ranged_Loops.cpp
@@ -0,0 +1,16 @@
+// Ranged Loops
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int array1[] = { 1,2,3,4,5,6 };
+
+    for (int n : array1)
+    {
+        cout << n << endl;
+    }
+
+}
