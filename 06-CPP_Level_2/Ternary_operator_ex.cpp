7bb7b6deedd64ddc81959ab4606bee42425cb548
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-09-11 13:41:48 +0300

    Add Ternary operator example in C++

---
+++ 06-CPP_Level_2/Ternary_operator_ex.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+	int num = 1;
+	string result;
+
+
+	result = (num > 0) ?  "Positive" : "Negative" ;
+
+	cout << "Number Is " << result << "\n";
+	
+
+}
+
