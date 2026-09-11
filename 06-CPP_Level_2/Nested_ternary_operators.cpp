890e8f6dacb4fe4c434eb031e2d2b27d3af9be4e
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-09-11 13:54:48 +0300

    Nested_ternary_operators.cpp
    Remove unnecessary blank line and ensure proper newline at end of file.

---
+++ 06-CPP_Level_2/Nested_ternary_operators.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+	int num = 0;
+	string result;
+
+
+	result = (num == 0) ?  "Zero" : ((num > 0) ? "Positive" : "Negative"); 
+
+	cout << "Number Is " << result << "\n";
+	
+
+}
+
+ 
