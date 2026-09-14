107d6bf45bb71e5a03bdb8c6f19e323bbf5e6d50
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-09-14 18:44:15 +0300

    Add example of automatic variables in C++
    Demonstrate the use of automatic type deduction in C++.

---
+++ 06-CPP_Level_2/Automatic_Variables.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+    
+
+int main()
+{
+    auto x = 10;     // Type Intger
+    auto y = 12.5;     // Type Float 
+    auto z = "Hi , I'm Mohamed :) \n";    // Type String 
+
+
+    cout << x << endl; 
+    cout << y << endl; 
+    cout << z << endl; 
+
+}
+
+
