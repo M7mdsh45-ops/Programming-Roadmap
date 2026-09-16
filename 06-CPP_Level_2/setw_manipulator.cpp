5ef9da5b19cd5a9b4f06bb787a8696c0cb5debac
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-09-17 01:08:52 +0300

    Add setw manipulator example in C++

---
+++ 06-CPP_Level_2/setw_manipulator.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+
+	cout << "---------|--------------------------------|---------|\n";
+	cout << "  Code   |              Name              |   Mark  |\n";
+	cout << "---------|--------------------------------|---------|\n";
+	
+
+	cout << setw(9) << "C101" << "|" << setw(32) << "Introduction To Programming 1" << "|" << setw(9) << "99" << "|" << endl;
+	cout << setw(9) << "C102" << "|" << setw(32) << "Computer HardWare" << "|" << setw(9) << "90" << "|" << endl;
+	cout << setw(9) << "C103454" << "|" << setw(32) << "Network " << "|" << setw(9) << "100" << "|" << endl;
+	cout << "---------|--------------------------------|---------|\n";
+
+}
+
+
