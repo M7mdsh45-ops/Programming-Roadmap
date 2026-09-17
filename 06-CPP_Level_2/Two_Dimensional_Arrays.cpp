d00573bd3a21a8277f9703e2a325a30902265c31
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-09-18 00:00:25 +0300

    Implement 10x10 two-dimensional array initialization
    This program initializes a 10x10 two-dimensional array and populates it with the product of its indices. It then prints the array in a formatted manner.

---
+++ 06-CPP_Level_2/Two_Dimensional_Arrays.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+ 
+	// int x[Rows][Cols] :
+
+	int x[10][10] = { {} };
+
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			 x[i][j] = (i + 1) * (j + 1) ;
+		}
+	}
+
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			 printf("%02d ",x[i][j]);
+		}
+
+		cout << endl;
+	}
+}
+
