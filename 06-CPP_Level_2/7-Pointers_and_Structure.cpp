bad3cd05ef89ba3cef0b7741f7a473e6638c4f1b
Mohamed Ahmed <m7mdsh45@gmail.com>
2026-09-22 05:20:27 +0300

    Add structure and pointer example for employee data

---
+++ 06-CPP_Level_2/7-Pointers_and_Structure.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct stEmployee
+{
+	string name;
+	int salary;
+
+};
+
+
+int main()
+{
+	
+	stEmployee Employee, *ptr;
+	
+	Employee.name = "Mohamed Ahmed";
+	Employee.salary = 5000;
+
+	cout << "Name : " << Employee.name << endl;
+	cout << "Salary : " << Employee.salary << endl;
+	cout << "\n-------------------\n";
+
+	ptr = &Employee;
+
+
+	cout << "\nUsing Pointer : \n";
+	cout << "Name : " << ptr->name << endl; //(*ptr).Name  == ptr->Name
+	cout << "Salary : " << ptr->salary << endl;
+
+	
+}
